284b67387c87a3cab110d6511d3ef0c051a07e20
Chris Vasterling <chrisvasterling@gmail.com>
2016-01-05 19:32:29 -0600

    Update

--- Files/Javascript/Main.js
+++ Files/Javascript/Main.js
@@ -1,3 +1,7 @@
 window.addEventListener("load", function () {
-	
+	slideShow();
 });
+function slideShow () {
+    var img1 = document.getElementById("exImg1"),
+        img2 = document.getElementById("exImg2");
+}
