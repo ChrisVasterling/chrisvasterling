5f618c889c38b488df26a331b85e2ac18ca2ffd8
Chris Vasterling <chrisvasterling@gmail.com>
2016-01-10 18:18:11 -0600

    Update

--- Files/Javascript/Main.js
+++ Files/Javascript/Main.js
@@ -4,34 +4,57 @@ window.addEventListener("load", function () {
 function slideShow() {
     var slide1 = document.getElementById("slide1"),
         slide2 = document.getElementById("slide2"),
+        slide3 = document.getElementById("slide3"),
+        slide4 = document.getElementById("slide4"),
+        slide5 = document.getElementById("slide5"),
         prev = document.getElementById("prev"),
         next = document.getElementById("next"),
         dig = 0;
 	slide2.style.display  = "none";
+    slide3.style.display  = "none";
+    slide4.style.display  = "none";
+    slide5.style.display  = "none";
     prev.addEventListener("click", slideShowPrev);
     next.addEventListener("click", slideShowNext);
-    
-}
-
-
-
-
-
-var img = 0;
-var imgselect = 0;
-function slideshow() {
-	if ( img >= 0 ) {
-		img = img + 1
-		imgselect = imgselect + 1
-		var ss  = document.getElementById("slideshow");
-		ss.style.backgroundImage = 'url(Files/Images/Slideshow/' + imgselect + '_opt.jpg)';
-		if ( imgselect == 5 || imgselect >= 5 ) {
-			imgselect = 0
-			img = 0
-		} else {
-			/**/
-		}
-	} else {
-		/**/
-	}
+    function slideShowNext() {
+        if ( dig == 0 ) {
+            slide1.style.display = "none";
+            slide2.style.display = "block";
+            slide3.style.display = "none";
+            slide4.style.display  = "none";
+            slide5.style.display  = "none";
+            dig = dig + 1;
+        } else if ( dig == 1 ) {
+            slide1.style.display = "none";
+            slide2.style.display = "none";
+            slide3.style.display = "block";
+            slide4.style.display  = "none";
+            slide5.style.display  = "none";
+            dig = dig + 1;
+        } else if ( dig == 2 ) {
+            slide1.style.display = "none";
+            slide2.style.display = "none";
+            slide3.style.display = "none";
+            slide4.style.display  = "block";
+            slide5.style.display  = "none";
+            dig = dig + 1;
+        } else if ( dig == 3 ) {
+            slide1.style.display = "none";
+            slide2.style.display = "none";
+            slide3.style.display = "none";
+            slide4.style.display  = "none";
+            slide5.style.display  = "block";
+            dig = dig + 1;
+        } else if ( dig == 4 ) {
+            slide1.style.display = "block";
+            slide2.style.display = "none";
+            slide3.style.display = "none";
+            slide4.style.display  = "none";
+            slide5.style.display  = "none";
+            dig = 0;
+        }
+    }
+    function slideShowPrev() {
+        
+    }
 }
